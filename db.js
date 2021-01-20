0aab51a53a09fe30a76416f202779fbea08ec1b4
Belen Paredes <belen_ub_@hotmail.com>
2021-01-20 20:23:40 +0000

    test get order

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const DB_URL = (process.env.MONGO_URL || 'mongodb://db/test');
+//const DB_URL = (process.env.MONGO_URL || 'mongodb://db/test');
 
 const dbConnect = function() {
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error: '));
-    return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+   // return mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    return mongoose.connect("mongodb+srv://bel_package:fis2021@cluster0.m9znp.mongodb.net/<dbname>?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 }
 
 module.exports = dbConnect;
